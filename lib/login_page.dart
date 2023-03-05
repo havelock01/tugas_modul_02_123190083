fc65fb7f3529c20402a500cf4f978eb085c1d3c4
Muhammad Rizqi <mrizqi0153@gmail.com>
2023-03-05 22:50:08 +0700

    Done All Task
    Buang leading agar sesuai dengan AppBar tugas, mengatur container, buat folder images, masukin gambar icon flutter di images, dan atur file pubpec.yaml agar asset images di load.

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -7,26 +8,55 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(icon: new Icon(Icons.home), iconSize: 24.0, onPressed: () {}),
         title: Text('Login Screen'),
       ),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Image.asset('assets/images/dartFlutterBird.png'),
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Username',
+            Container(
+              margin: EdgeInsets.all(20),
+              child: Image.asset('images/fluttericon.png'),
+            ),
+            Container(
+              width: 450.0,
+              margin: EdgeInsets.only(top: 20),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(50.0)),
+                  hintText: 'Email',
+                ),
               ),
             ),
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Username',
+            Container(
+              width: 450.0,
+              margin: EdgeInsets.only(top: 10),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(50.0)),
+                  hintText: 'Password',
+                ),
               ),
             ),
-            ElevatedButton(onPressed: (){}, child: Text('Login'))
+            Container(
+              margin: EdgeInsets.only(top: 30),
+              child: ElevatedButton(
+                onPressed: (){},
+                child: Text('Log In'),
+                style: ButtonStyle(
+                  minimumSize: MaterialStateProperty.all(const Size(450, 40)),
+                ),
+              ),
+            ),
+            TextButton(
+                onPressed: (){},
+                child: Text('Forgot Password?',
+                  style: TextStyle(
+                      color: Colors.grey
+                  ),
+                )
+            ),
           ],
         ),
       ),
